ba850adbe8f748faa2cee3c425b3bda2c397a8b9
osamahanan <hananusama064@gmail.com>
2025-09-04 23:28:03 +0500

    database added

--- Db.js
+++ Db.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
  const connectDb = async() =>{
 
 try {
-    await mongoose.connect("mongodb://localhost:27017/SigmaBlog")
+    await mongoose.connect("mongodb+srv://osama:_OsamaHanan@cluster0.ynihopm.mongodb.net/SigmaBlog?retryWrites=true&w=majority&appName=Cluster0")
     console.log("Database is connected");
+    
     
 } catch (error) {
   console.log(error);
